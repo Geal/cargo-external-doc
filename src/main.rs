28ebf7ba9e2c0316083bebb151b6aa52756276ba
Jelte Fennema <github-tech@jeltef.nl>
2018-03-14 09:06:06 +0100

    Rustfmt source code

--- src/main.rs
+++ src/main.rs
@@ -15,39 +15,54 @@ fn get_package_name() -> String {
     let mut cargo = Command::new("cargo");
     cargo.arg("read-manifest");
     let stdout = String::from_utf8(cargo.output().expect("`cargo metadata` did not run").stdout)
-                     .expect("invalid encoding");
+        .expect("invalid encoding");
     let value: Value = serde_json::from_str(&stdout).expect("invalid JSON metadata");
     let main: &Map<String, Value> = value.as_object().expect("top level value is not an object");
     String::from(main["name"].as_str().expect("invalid main string")).replace('-', "_")
 }
 
-fn generate_wrapper(name: &str) -> (NamedTempFile,NamedTempFile) {
-  let handlebars = Handlebars::new();
-  let before = include_str!("../templates/before.html");
-  let after  = include_str!("../templates/after.html");
+fn generate_wrapper(name: &str) -> (NamedTempFile, NamedTempFile) {
+    let handlebars = Handlebars::new();
+    let before = include_str!("../templates/before.html");
+    let after = include_str!("../templates/after.html");
 
-  let mut hash = HashMap::new();
-  hash.insert(String::from("name"), String::from(name));
+    let mut hash = HashMap::new();
+    hash.insert(String::from("name"), String::from(name));
 
-  let before_html = handlebars.template_render(before, &hash).expect("could not render the HTML prefix file");
-  let after_html  = handlebars.template_render(after, &hash).expect("could not render the HTML suffix file");
+    let before_html = handlebars
+        .template_render(before, &hash)
+        .expect("could not render the HTML prefix file");
+    let after_html = handlebars
+        .template_render(after, &hash)
+        .expect("could not render the HTML suffix file");
 
-  let mut before_file = NamedTempFile::new().expect("could not create temporary file");
-  let mut after_file  = NamedTempFile::new().expect("could not create temporary file");
+    let mut before_file = NamedTempFile::new().expect("could not create temporary file");
+    let mut after_file = NamedTempFile::new().expect("could not create temporary file");
 
-  before_file.write_all(before_html.as_bytes()).expect("could not write the HTML prefix file");
-  before_file.flush().expect("could not write the HTML prefix file");
-  after_file.write_all(after_html.as_bytes()).expect("could not write the HTML prefix file");
-  after_file.flush().expect("could not write the HTML suffix file");
+    before_file
+        .write_all(before_html.as_bytes())
+        .expect("could not write the HTML prefix file");
+    before_file
+        .flush()
+        .expect("could not write the HTML prefix file");
+    after_file
+        .write_all(after_html.as_bytes())
+        .expect("could not write the HTML prefix file");
+    after_file
+        .flush()
+        .expect("could not write the HTML suffix file");
 
-  (before_file, after_file)
+    (before_file, after_file)
 }
 
 ///test
 fn main() {
     let mut cargo = Command::new("cargo");
     cargo.arg("doc");
-    let stdout = cargo.output().map(|o| o.stdout).expect("could not open cargo output");
+    let stdout = cargo
+        .output()
+        .map(|o| o.stdout)
+        .expect("could not open cargo output");
     let result: String = String::from_utf8(stdout).expect("cargo command failed");
     //println!("cargo result: {}", result);
 
@@ -59,7 +74,7 @@ fn main() {
 
     let (before_html, after_html) = generate_wrapper(&crate_name);
     let before_path = before_html.path();
-    let after_path  = after_html.path();
+    let after_path = after_html.path();
     //println!("generating temporary HTML files at {:?} and {:?}", before_path, after_path);
 
     let doc_files = fs::read_dir("./doc").expect("could not read directory content");
@@ -73,10 +88,10 @@ fn main() {
             // FIXME: the debug folder has to be there, then :/
             test.arg("./target/debug/");
             let test_stdout = test.output()
-                                  .map(|o| o.stdout)
-                                  .expect("could not open rustdoc output");
-            let test_result: String = String::from_utf8(test_stdout)
-                                          .expect("rustdoc command failed");
+                .map(|o| o.stdout)
+                .expect("could not open rustdoc output");
+            let test_result: String =
+                String::from_utf8(test_stdout).expect("rustdoc command failed");
             //println!("rustdoc --test result: {}", test_result);
 
             let mut rustdoc = Command::new("rustdoc");
@@ -97,12 +112,12 @@ fn main() {
             // FIXME: the debug folder has to be there, then :/
             rustdoc.arg("./target/debug/");
             rustdoc.arg("-v");
-            let doc_stdout = rustdoc.output()
-                                    .map(|o| o.stdout)
-                                    .expect("could not open rustdoc output");
+            let doc_stdout = rustdoc
+                .output()
+                .map(|o| o.stdout)
+                .expect("could not open rustdoc output");
             let doc_result: String = String::from_utf8(doc_stdout).expect("rustdoc command failed");
             //println!("rustdoc result: {}", doc_result);
         }
     }
 }
-
